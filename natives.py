a6737c12b9417b494e02750d8f3e163a42f0e874
ayoung1 <aaronjyoung1@gmail.com>
2016-10-11 20:56:18 -0700

    Initial compiler and natives:

---
+++ natives.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+import lambdalang
+
+def setup_natives(env):
+	natives = [
+			{
+			'name':'print',
+			'func':(lambda x='':print(stringify(x), end=''))
+			},
+			{
+			'name':'println',
+			'func':(lambda x='':print(stringify(x), end='\n'))
+			},
+			{
+			'name':'str',
+			'func':(lambda x='':str(x))
+			},
+			{
+			'name':'int',
+			'func':(lambda x:int(x))
+			},
+			{
+			'name':'float',
+			'func':(lambda x:float(x))
+			},
+			{
+			'name':'input',
+			'func':(lambda x='':raw_input(x))
+			}
+		]
+
+	for x in natives:
+		env.define(x['name'], x['func'])
+def stringify(value):
+	if isinstance(value, (list, tuple)):
+		a = []
+		for x in value:
+			a.append(x['value'])
+		return a
+	elif value == None:
+		return 'null'
+	else:
+		try:
+			return value['value']
+		except:
+			return value
